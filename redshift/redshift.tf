5e6e8ae5eae127304251ef2fb3cf3132da777450
DumbMachine <ratin.kumar.2k@gmail.com>
2023-05-26 22:21:09 +0530

    added docdb redshift zerotier

---
+++ redshift/redshift.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    adaptive = {
+      source  = "adaptive-scale/adaptive"
+      version = "0.0.6"
+    }
+  }
+}
+
+provider "adaptive" {}
+
+resource "adaptive_resource" "postgres_test" {
+  type          = "redshift"
+  name          = "pagila42"
+  host          = "example.com"
+  port          = "5432"
+  username      = "postgres"
+  password      = "<password>"
+  database_name = "pagila"
+}
